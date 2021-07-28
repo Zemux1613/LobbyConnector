dd05ac440112559c2cbc4d665b3bda525422c9e6
Zemux1613 <42244022+Zemux1613@users.noreply.github.com>
2021-07-28 13:23:19 +0200

    Eliminated ifs and cleaned up the code

--- src/main/java/de/combit/lobby/listener/InventoryClickListener.java
+++ src/main/java/de/combit/lobby/listener/InventoryClickListener.java
@@ -17,82 +17,32 @@ public class InventoryClickListener implements Listener {
 
     @EventHandler
     public void onClick(InventoryClickEvent event) {
-        Player player = ((Player) event.getWhoClicked());
+        final Player player = ((Player) event.getWhoClicked());
         FileConfiguration configuration = LobbyConnector.instance.getSettingsConfig();
-        if (event.getInventory().getName().equalsIgnoreCase(configuration.getString("Gui-1.Name"))) {
-            event.setCancelled(true);
-            ICloudPlayer iCloudPlayer = CloudAPI.getInstance().getCloudPlayerManager().getCachedCloudPlayer(player.getUniqueId());
-            String serviceRaw = event.getCurrentItem().getItemMeta().getDisplayName();
-            String service = serviceRaw.replaceAll("§e", "").replaceAll("§7", "");
-            iCloudPlayer.connect(CloudAPI.getInstance().getCloudServiceManager().getCloudServiceByName(service));
+        
+        switch(event.getInventory().getName().toLowerCase()){
+        
+            case configuration.getString("Gui-1.Name")).toLowerCase():
+                case configuration.getString("Gui-2.Name")).toLowerCase():
+                case configuration.getString("Gui-3.Name")).toLowerCase():
+                case configuration.getString("Gui-4.Name")).toLowerCase():
+                case configuration.getString("Gui-5.Name")).toLowerCase():
+                case configuration.getString("Gui-6.Name")).toLowerCase():
+                case configuration.getString("Gui-7.Name")).toLowerCase():
+                case configuration.getString("Gui-8.Name")).toLowerCase():
+                case configuration.getString("Gui-9.Name")).toLowerCase():
+                case configuration.getString("Gui-10.Name")).toLowerCase():
+                
+                    event.setCancelled(true);
+                    ICloudPlayer iCloudPlayer = CloudAPI.getInstance().getCloudPlayerManager().getCachedCloudPlayer(player.getUniqueId());
+                    final String serviceRaw = event.getCurrentItem().getItemMeta().getDisplayName();
+                    final String service = serviceRaw.replaceAll("§e", "").replaceAll("§7", "");
+                    iCloudPlayer.connect(CloudAPI.getInstance().getCloudServiceManager().getCloudServiceByName(service));
+                
+                break;
+                
         }
-        if (event.getInventory().getName().equalsIgnoreCase(configuration.getString("Gui-2.Name"))) {
-            event.setCancelled(true);
-            ICloudPlayer iCloudPlayer = CloudAPI.getInstance().getCloudPlayerManager().getCachedCloudPlayer(player.getUniqueId());
-            String serviceRaw = event.getCurrentItem().getItemMeta().getDisplayName();
-            String service = serviceRaw.replaceAll("§e", "").replaceAll("§7", "");
-            iCloudPlayer.connect(CloudAPI.getInstance().getCloudServiceManager().getCloudServiceByName(service));
-        }
-        if (event.getInventory().getName().equalsIgnoreCase(configuration.getString("Gui-3.Name"))) {
-            event.setCancelled(true);
-            ICloudPlayer iCloudPlayer = CloudAPI.getInstance().getCloudPlayerManager().getCachedCloudPlayer(player.getUniqueId());
-            String serviceRaw = event.getCurrentItem().getItemMeta().getDisplayName();
-            String service = serviceRaw.replaceAll("§e", "").replaceAll("§7", "");
-            iCloudPlayer.connect(CloudAPI.getInstance().getCloudServiceManager().getCloudServiceByName(service));
-        }
-        if (event.getInventory().getName().equalsIgnoreCase(configuration.getString("Gui-4.Name"))) {
-            event.setCancelled(true);
-            ICloudPlayer iCloudPlayer = CloudAPI.getInstance().getCloudPlayerManager().getCachedCloudPlayer(player.getUniqueId());
-            String serviceRaw = event.getCurrentItem().getItemMeta().getDisplayName();
-            String service = serviceRaw.replaceAll("§e", "").replaceAll("§7", "");
-            iCloudPlayer.connect(CloudAPI.getInstance().getCloudServiceManager().getCloudServiceByName(service));
-        }
-        if (event.getInventory().getName().equalsIgnoreCase(configuration.getString("Gui-5.Name"))) {
-            event.setCancelled(true);
-            ICloudPlayer iCloudPlayer = CloudAPI.getInstance().getCloudPlayerManager().getCachedCloudPlayer(player.getUniqueId());
-            String serviceRaw = event.getCurrentItem().getItemMeta().getDisplayName();
-            String service = serviceRaw.replaceAll("§e", "").replaceAll("§7", "");
-            iCloudPlayer.connect(CloudAPI.getInstance().getCloudServiceManager().getCloudServiceByName(service));
-        }
-        if (event.getInventory().getName().equalsIgnoreCase(configuration.getString("Gui-6.Name"))) {
-            event.setCancelled(true);
-            ICloudPlayer iCloudPlayer = CloudAPI.getInstance().getCloudPlayerManager().getCachedCloudPlayer(player.getUniqueId());
-            String serviceRaw = event.getCurrentItem().getItemMeta().getDisplayName();
-            String service = serviceRaw.replaceAll("§e", "").replaceAll("§7", "");
-            iCloudPlayer.connect(CloudAPI.getInstance().getCloudServiceManager().getCloudServiceByName(service));
-        }
-        if (event.getInventory().getName().equalsIgnoreCase(configuration.getString("Gui-7.Name"))) {
-            event.setCancelled(true);
-            ICloudPlayer iCloudPlayer = CloudAPI.getInstance().getCloudPlayerManager().getCachedCloudPlayer(player.getUniqueId());
-            String serviceRaw = event.getCurrentItem().getItemMeta().getDisplayName();
-            String service = serviceRaw.replaceAll("§e", "").replaceAll("§7", "");
-            iCloudPlayer.connect(CloudAPI.getInstance().getCloudServiceManager().getCloudServiceByName(service));
-        }
-        if (event.getInventory().getName().equalsIgnoreCase(configuration.getString("Gui-8.Name"))) {
-            event.setCancelled(true);
-            ICloudPlayer iCloudPlayer = CloudAPI.getInstance().getCloudPlayerManager().getCachedCloudPlayer(player.getUniqueId());
-            String serviceRaw = event.getCurrentItem().getItemMeta().getDisplayName();
-            String service = serviceRaw.replaceAll("§e", "").replaceAll("§7", "");
-            iCloudPlayer.connect(CloudAPI.getInstance().getCloudServiceManager().getCloudServiceByName(service));
-        }
-        if (event.getInventory().getName().equalsIgnoreCase(configuration.getString("Gui-9.Name"))) {
-            event.setCancelled(true);
-            ICloudPlayer iCloudPlayer = CloudAPI.getInstance().getCloudPlayerManager().getCachedCloudPlayer(player.getUniqueId());
-            String serviceRaw = event.getCurrentItem().getItemMeta().getDisplayName();
-            String service = serviceRaw.replaceAll("§e", "").replaceAll("§7", "");
-            iCloudPlayer.connect(CloudAPI.getInstance().getCloudServiceManager().getCloudServiceByName(service));
-        }
-        if (event.getInventory().getName().equalsIgnoreCase(configuration.getString("Gui-10.Name"))) {
-            event.setCancelled(true);
-            ICloudPlayer iCloudPlayer = CloudAPI.getInstance().getCloudPlayerManager().getCachedCloudPlayer(player.getUniqueId());
-            String serviceRaw = event.getCurrentItem().getItemMeta().getDisplayName();
-            String service = serviceRaw.replaceAll("§e", "").replaceAll("§7", "");
-            iCloudPlayer.connect(CloudAPI.getInstance().getCloudServiceManager().getCloudServiceByName(service));
-        }
-
-
-
-
+        
     }
 
 }
